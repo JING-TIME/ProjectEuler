91d9772563f4a77014b00f87d5a5a3a68429c004
Jenny Zhang <changchongning@gmail.com>
2014-09-29 19:51:12 +0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+  long i,num=600851475143;
+  for (i=2; i<num/2; i++)
+  {
+      while(num % i == 0) 
+          num /= i;
+  }
+  printf("%d\n", num);
+}
+
+/*这是参考zguangyu0000写的，而且最初在 for 和 while 的用法上面弄错了。注意for后面括号里是分号
